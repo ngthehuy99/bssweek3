c9ea26094acb8ebf7e0a45a0771facb42a181dfc
ngthehuy99 <ngthehuy99@gmail.com>
2022-05-11 17:14:41 +0700

    commit

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Koa = require('koa');
+const Router = require('@koa/router');
+const render = require('koa-ejs');
+const path = require('path');
+
+const app = new Koa();
+const router = new Router();
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'index',
+  viewExt: 'html',
+  cache: false,
+  debug: true
+});
+
+router.get('koa-example', '/', (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(1234);
